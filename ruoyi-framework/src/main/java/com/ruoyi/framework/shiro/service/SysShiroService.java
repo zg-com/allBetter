57f7eff46c20f0e978fc0c533d13a18fd7d83fbf
诸葛 <15507007+zgcom@user.noreply.gitee.com>
2026-03-27 14:17:26 +0800

    框架搭建

---
+++ ruoyi-framework/src/main/java/com/ruoyi/framework/shiro/service/SysShiroService.java
@@ -0,0 +1,53 @@
+package com.ruoyi.framework.shiro.service;
+
+import java.io.Serializable;
+import org.apache.shiro.session.Session;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import com.ruoyi.common.core.session.OnlineSession;
+import com.ruoyi.common.utils.StringUtils;
+import com.ruoyi.system.domain.SysUserOnline;
+import com.ruoyi.framework.shiro.session.OnlineSessionFactory;
+import com.ruoyi.system.service.ISysUserOnlineService;
+
+/**
+ * 会话db操作处理
+ * 
+ * @author ruoyi
+ */
+@Component
+public class SysShiroService
+{
+    @Autowired
+    private ISysUserOnlineService onlineService;
+
+    @Autowired
+    private OnlineSessionFactory onlineSessionFactory;
+
+    /**
+     * 删除会话
+     *
+     * @param onlineSession 会话信息
+     */
+    public void deleteSession(OnlineSession onlineSession)
+    {
+        onlineService.deleteOnlineById(String.valueOf(onlineSession.getId()));
+    }
+
+    /**
+     * 获取会话信息
+     *
+     * @param sessionId
+     * @return
+     */
+    public Session getSession(Serializable sessionId)
+    {
+        SysUserOnline userOnline = onlineService.selectOnlineById(String.valueOf(sessionId));
+        return StringUtils.isNull(userOnline) ? null : createSession(userOnline);
+    }
+
+    public Session createSession(SysUserOnline userOnline)
+    {
+        return onlineSessionFactory.createSession(userOnline);
+    }
+}
